2b47a1be1502bce197e8f2edaa1bb6b437bdd719
Ryano <ryan.justin.taylor@gmail.com>
2015-07-06 13:40:33 -0700

    added state routing

--- html/script.js
+++ html/script.js
@@ -1,13 +1,23 @@
 'use strict';
 
 angular.module('app', ['ui.router'])
+.config(function($urlRouterProvider, $stateProvider, $locationProvider) {
+  $urlRouterProvider.otherwise('/table');
 
+  $stateProvider
+  .state('table', {
+    url: '/table',
+    templateUrl: 'table.html',
+    controller: 'TableCtrl'
+  });
+})
 .controller('TableCtrl', function($scope, TableService){
   var loadTable = function(){
     TableService.getAllData()
     .success(function(data){
       $scope.data = data;
       $scope.headers = Object.keys(data[0]);
+      $scope.CellColor = TableService.CellColor;
     })
     .catch(function(error){
       console.log(error);
@@ -15,17 +25,13 @@ angular.module('app', ['ui.router'])
   }
   loadTable();
 
-  $scope.CellColor = {
-    'Yes': 'green',
-    'No': 'red',
-    'Partial': 'orange'
-  };
   $scope.highlight = function(col, row){
     if(this.column !== 'Feature/Engine'){
       $scope.colHover = col;
     }
     $scope.rowHover = row;
   }
+
   $scope.resetHighlighting = function(){
     $scope.rowHover = false;
     $scope.colHover = false;
@@ -35,6 +41,11 @@ angular.module('app', ['ui.router'])
   return {
     getAllData: function(){
       return $http.get('/data');
+    },
+    CellColor: {
+      'Yes': 'green',
+      'No': 'red',
+      'Partial': 'orange'
     }
   };
 });
